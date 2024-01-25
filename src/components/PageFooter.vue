88166b6e6da872cc87bdfe552fb45affa1c2f8f0
ZiyadBouazara <ziyad.bouazara1@ulaval.ca>
2024-01-24 20:51:24 -0500

    Footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="page-footer mt-auto py-3 bg-light">
+    <div class="container">
+      <span class="text-muted">UFood © 2024</span>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
+
+<script></script>
