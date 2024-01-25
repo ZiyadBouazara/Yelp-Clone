1309f9f2ce5b839326774cdeefc2fc49ac09d7b3
ZiyadBouazara <ziyad.bouazara1@ulaval.ca>
2024-01-24 19:52:18 -0500

    Added Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import { createApp } from "vue";
 import { router } from "./router";
 import App from "./App.vue";
