ddd9f5a967321e4720623c99723d909703cdedda
Sami Sabir <sasab21@ulaval.ca>
2024-02-08 19:10:47 -0500

    non usage des bundles bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import { createApp } from "vue";
 import { router } from "./router";
 import App from "./App.vue";
