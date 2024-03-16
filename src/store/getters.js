39fce504e067a1603a7507785edb9bd03d29e2b0
abdellahhn <abdellahhanane44@gmail.com>
2024-03-16 14:02:44 -0400

    divided store vue X

---
+++ src/store/getters.js
@@ -0,0 +1,16 @@
+export const getters = {
+  getTotalRestaurant: (state) => state.totalRestaurant,
+  getPrice: (state) => state.price,
+  getGenres: (state) => state.genres,
+  getSearchTermGenre: (state) => state.searchTermGenre,
+  getSearchTerm: (state) => state.searchTerm,
+  getRestaurants: (state) => state.restaurants,
+  getUsers: (state) => state.users,
+  getUserFavorites: (state) => state.favorites,
+  getImageIndex: (state) => state.imageIndex,
+  getLoggedInUser: (state) => state.loggedInUser,
+  getVisits: (state) => state.visits,
+  getRestaurantById: (state) => (restaurantId) => {
+    return state.restaurants.find((r) => r.id === restaurantId);
+  },
+};
